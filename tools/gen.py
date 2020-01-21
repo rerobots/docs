386ccacd33a0ef9c9fe442d21f93c6aab819a365
Scott C. Livingston <scott@rerobots.net>
2020-01-20 16:11:49 -0800

    prefer HTTPS when available for external links

--- tools/gen.py
+++ tools/gen.py
@@ -68,7 +68,7 @@ SUFFIX="""</div>
   <div class="container"><div class="row">
     <div class="col-md-6 col-sm-6 text-muted">
       Copyright &copy; 2020 rerobots, Inc.<br />
-      This work is licensed under a <a rel="license" href="http://creativecommons.org/licenses/by/4.0/" id="commons-license">Creative Commons Attribution 4.0 International License</a>.
+      This work is licensed under a <a rel="license" href="https://creativecommons.org/licenses/by/4.0/" id="commons-license">Creative Commons Attribution 4.0 International License</a>.
     </div>
     <div class="col-md-6 col-sm-6 text-muted">
       <span id="update-date">updated {DATESTAMP}</span>
