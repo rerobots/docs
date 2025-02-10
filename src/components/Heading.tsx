7630d8146b8ba1c0050e321486f51659c4a21047
Scott C. Livingston <scott@rerobots.net>
2025-02-09 20:19:14 -0800

    section header permalinks

--- src/components/Heading.tsx
+++ src/components/Heading.tsx
@@ -9,9 +9,13 @@ interface HeadingProps {
 export function Heading({ children, level }: HeadingProps)
 {
     const anchor = children.toLowerCase().split(/ +/).join('-')
+    const wrappedChildren = (<>
+        <span>{children}</span>
+        <a href={'#' + anchor} className='permalink' aria-label='permalink'></a>
+    </>);
     return React.createElement(
         `h${level}`,
         {'id': anchor},
-        children
+        wrappedChildren
     );
 }
