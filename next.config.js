fec3524d6cfccd6ffed44cebfbd8d91a8fd29aef
Scott C. Livingston <scott@rerobots.net>
2025-07-20 16:59:08 -0700

    enable locale handling by Next.js

--- next.config.js
+++ next.config.js
@@ -3,5 +3,10 @@ const withMarkdoc = require('@markdoc/next.js');
 module.exports = withMarkdoc({
     schemaPath: 'src/markdoc',
 })({
+    i18n: {
+        defaultLocale: 'en-US',
+        locales: ['en-US', 'es'],
+        localeDetection: true,
+    },
     pageExtensions: ['ts', 'tsx', 'md'],
 });
