ff93bfe31b1db98a94a995afac1a1646f5fbfece
Scott C. Livingston <scott@rerobots.net>
2025-08-09 16:11:20 -0700

    Revert "enable locale handling by Next.js"
    This reverts commit fec3524d6cfccd6ffed44cebfbd8d91a8fd29aef.
    
    Instead of using Nextjs Pages Router built-in languages handling,
    we will use a combination of lang/ directories, manual selection
    by the user, and automatic handling of Accept-Language headers.

--- next.config.js
+++ next.config.js
@@ -3,10 +3,5 @@ const withMarkdoc = require('@markdoc/next.js');
 module.exports = withMarkdoc({
     schemaPath: 'src/markdoc',
 })({
-    i18n: {
-        defaultLocale: 'en-US',
-        locales: ['en-US', 'es'],
-        localeDetection: true,
-    },
     pageExtensions: ['ts', 'tsx', 'md'],
 });
