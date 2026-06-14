5c5aa08fb5d4420e68528cffab4022874f5092c5
Scott C. Livingston <scott@rerobots.net>
2026-06-13 23:53:18 -0700

    use new method of Next.js static export
    https://nextjs.org/docs/app/guides/static-exports

--- next.config.js
+++ next.config.js
@@ -3,5 +3,6 @@ const withMarkdoc = require('@markdoc/next.js');
 module.exports = withMarkdoc({
     schemaPath: 'src/markdoc',
 })({
+    output: 'export',
     pageExtensions: ['ts', 'tsx', 'md'],
 });
